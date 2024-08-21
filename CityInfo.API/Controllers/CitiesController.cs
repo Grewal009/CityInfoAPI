ce63b7e7863948686ec6e4540ed93b066c34dfda
Aneel Grewal <aneel.grewal@hotmail.com>
2024-08-21 19:25:32 +0200

    GET Cities & GET CIties/id endpoints

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetCities()
+    {
+        return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public JsonResult GetCity(int id)
+    {
+        return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault
+            (c => c.Id == id));
+    }
+
+
+}
