aa222a9b2500840cd680766040afc79e337c104d
Aneel Grewal <aneel.grewal@hotmail.com>
2024-08-23 03:51:41 +0200

    getting a file or downloading a file via API

---
+++ CityInfo.API/Controllers/FileController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider
+            _fileExtensionContentTypeProvider;
+
+        public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "hello.webp";
+            //var pathToFile = "Git.pages";
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType , Path.GetFileName(pathToFile));
+        }
+    }
+}
